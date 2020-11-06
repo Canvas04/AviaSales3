fcf9465b3354107d1bab961895349dcbf00bf2aa
Ruslan04071996 <ruslan040796@mail.ru>
2020-11-06 12:43:28 +0300

    Поместил чекбоксы внутри табов

--- src/containers/tab1/tab1.js
+++ src/containers/tab1/tab1.js
@@ -1,6 +1,12 @@
 import React ,{useEffect} from 'react';
 import  {useDispatch,useSelector} from 'react-redux';
+import { Checkbox } from "antd";
 import { speedTickets } from '../../action/filters';
+import {pressCheck} from '../../action/checkboxes'
+import {getCheckId,getCheckLabel,getCheck} from './extra';
+import classes from './tab1.module.scss';
+import './tab1.scss';
+const {aside,asideHeader} = classes;
 
 export default function Tab1 () {
     const dispatch = useDispatch();
@@ -10,6 +16,27 @@ const tickets = useSelector(store => {
     useEffect(() => {
 dispatch(speedTickets(tickets))
     })
+
+    const stateCheckboxes = useSelector((store) => store.checkboxes.items);
+
+    const checkboxes = stateCheckboxes.map((el) => {
+        return (
+          <Checkbox
+            key={el.id}
+            onChange={() =>
+              dispatch(pressCheck(getCheckId(el.id, stateCheckboxes),getCheckLabel(el.id, stateCheckboxes),tickets))
+            }
+            checked={getCheck(el.id, stateCheckboxes)}
+          >
+            {getCheckLabel(el.id, stateCheckboxes)}
+          </Checkbox>
+        );
+      });
+    
     return <>
+    <aside  className={aside}>
+        <h1 className={asideHeader}>Количество пересадок</h1>
+        {checkboxes}
+      </aside> 
     </>
 }
--- src/containers/tab2/tab2.js
+++ src/containers/tab2/tab2.js
@@ -1,16 +1,48 @@
-import React,{useEffect} from 'react';
-import  {useDispatch,useSelector} from 'react-redux';
-import { cheapTickets } from '../../action/filters';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cheapTickets } from "../../action/filters";
+import { Checkbox } from "antd";
+import { pressCheck } from "../../action/checkboxes";
+import { getCheckId, getCheckLabel, getCheck } from "../tab1/extra";
+import classes from "../tab1/tab1.module.scss";
+import "../tab1/tab1.scss";
 
-export default function Tab2 () {
-     const dispatch = useDispatch();
-    const tickets = useSelector(store => {
-        return store.loadTickets.tickets;
-    })
-    useEffect(() => {
-        dispatch(cheapTickets(tickets))
-    })
-    return <>
-    
-    </>
+const { aside, asideHeader } = classes;
+
+export default function Tab2() {
+  const dispatch = useDispatch();
+  const tickets = useSelector((store) => {
+    return store.loadTickets.tickets;
+  });
+  useEffect(() => {
+    dispatch(cheapTickets(tickets));
+  });
+  const stateCheckboxes = useSelector((store) => store.checkboxes.items);
+
+  const checkboxes = stateCheckboxes.map((el) => {
+    return (
+      <Checkbox
+        key={el.id}
+        onChange={() =>
+          dispatch(
+            pressCheck(
+              getCheckId(el.id, stateCheckboxes),
+              getCheckLabel(el.id, stateCheckboxes),
+              tickets
+            )
+          )
+        }
+        checked={getCheck(el.id, stateCheckboxes)}
+      >
+        {getCheckLabel(el.id, stateCheckboxes)}
+      </Checkbox>
+    );
+  });
+
+  return <>
+  <aside  className={aside}>
+        <h1 className={asideHeader}>Количество пересадок</h1>
+        {checkboxes}
+      </aside> 
+  </>;
 }
